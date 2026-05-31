dce388bf571ae206c6f5f85a9d578465bd74b169
luffy-dev24 <tthimmagalla@gmail.com>
2026-05-31 07:57:02 +0530

    created API's for inserting and retrive the employees data

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('add/',views.AddEmployee.as_view(),name='add-employee'),
+]
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/employees/views.py
+++ backend/employees/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Employees
+from .serializer import EmployeesSerializer
+from rest_framework import status
 # Create your views here.
+class AddEmployee(APIView):
+    def get(self,request):
+        employees = Employees.objects.all()
+        serializer_obj = EmployeesSerializer(employees,many=True)
+        return Response(serializer_obj.data,status=status.HTTP_200_OK)
+    def post(self,request):
+        serializer_obj  = EmployeesSerializer(data=request.data)
+        if serializer_obj.is_valid():
+            serializer_obj.save()
+            return Response(serializer_obj.data,status=status.HTTP_201_CREATED)
+        return Response(serializer_obj.errors,status=status.HTTP_400_BAD_REQUEST)
