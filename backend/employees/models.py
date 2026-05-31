1c83e2fa38988233690170dc868199bc32191949
luffy-dev24 <tthimmagalla@gmail.com>
2026-05-31 08:09:03 +0530

    added two columns and altered the name column in employees model

--- backend/employees/models.py
+++ backend/employees/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Employees(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,unique=True)
     age = models.IntegerField()
+    salary = models.IntegerField(null=True,blank=True)
+    profile_pic = models.ImageField(upload_to='images/',null=True,blank=True)
     
     def __str__(self):
         return self.name
